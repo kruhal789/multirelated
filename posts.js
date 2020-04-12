03e5911e00ec489ae37a9198513ecfcdf75f7948
kruhal789 <63550044+kruhal789@users.noreply.github.com>
2020-04-13 04:25:42 +0530

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,1 +1,1 @@
-<script expr:src='&quot;/feeds/posts/default/-/&quot; + data:label.name + &quot;?alt=json-in-script&amp;callback=related_results_labels&amp;max-results=100&quot;'/>
+expr:src='&quot;/feeds/posts/default/-/&quot; + data:label.name + &quot;?alt=json-in-script&amp;callback=related_results_labels&amp;max-results=100&quot;'
